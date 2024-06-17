875cf4192b6e07cd8ec0ce8a68a9c57ac341f334
HeBianGu <908293466@qq.com>
2024-06-17 12:03:16 +0800

    11

---
+++ Source/Test/WPF-SSSS/H.Test.Demo/GetAgeConverter.cs
@@ -0,0 +1,22 @@
+﻿
+using HeBianGu.Service.Converter;
+namespace H.Test.Demo
+{
+    public class GetAgeConverter : MarkupValueConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                DateTime now = DateTime.Now;
+                int age = now.Year - dateTime.Year;
+                if (now.Month < dateTime.Month || (now.Month == dateTime.Month && now.Day < dateTime.Day))
+                {
+                    age--;
+                }
+                return age > 0 ? age : 0;
+            }
+            return null;
+        }
+    }
+}
