60bdd11454386de9ab90d15992136bf7c4bb42ce
HeBianGu <908293466@qq.com>
2024-07-03 01:25:31 +0800

    json

---
+++ Source/Test/H.Test.Demo4/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System.Runtime.Serialization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace H.Test.Demo4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Test test = new Test();
+            test.Name = "HeBianGu";
+
+            {
+                var txt = JsonConvert.SerializeObject(test);
+                System.Diagnostics.Debug.WriteLine(txt);
+            }
+            {
+                var txt = JsonConvert.SerializeObject(test, Formatting.Indented);
+                System.Diagnostics.Debug.WriteLine(txt);
+            }
+            {
+                // 序列化接口 1 TypeNameHandling.Auto 2 序列化反序列化都用jsonSerializerSettings
+                List<ITest> tests = new List<ITest>();
+                tests.Add(new Test());
+                tests.Add(new MyTest());
+
+                Data data = new Data();
+                data.Tests = tests;
+
+                JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings();
+                jsonSerializerSettings.Formatting = Formatting.Indented;
+                jsonSerializerSettings.TypeNameHandling = TypeNameHandling.Auto;
+                jsonSerializerSettings.ContractResolver = new DefaultContractResolver()
+                {
+                    NamingStrategy=new DefaultNamingStrategy()
+                };
+                jsonSerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                jsonSerializerSettings.StringEscapeHandling=StringEscapeHandling.Default;
+                var txt = JsonConvert.SerializeObject(data, jsonSerializerSettings); 
+                System.Diagnostics.Debug.WriteLine(txt);
+                var datas = JsonConvert.DeserializeObject<Data>(txt, jsonSerializerSettings);
+             
+            }
+        }
+    }
+
+    public interface ITest
+    {
+
+    }
+
+    public class Data
+    {
+        public List<ITest> Tests { get; set; } = new List<ITest>();
+    }
+
+    public abstract class TestBase : ITest
+    {
+        public string ID { get; set; }
+    }
+    public class Test : TestBase
+    {
+        public string Name { get; set; }
+        //public HorizontalAlignment HorizontalAlignment { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    }
+
+    public class MyTest : TestBase
+    {
+        public string Member2 { get; set; }
+        public string Member3 { get; set; }
+        public string Member4 { get; set; }
+
+        [OnSerializing]
+        internal void OnSerializingMethod(StreamingContext context)
+        {
+            Member2 = "This value went into the data file during serialization.";
+        }
+
+        [OnSerialized]
+        internal void OnSerializedMethod(StreamingContext context)
+        {
+            Member2 = "This value was reset after serialization.";
+        }
+
+        [OnDeserializing]
+        internal void OnDeserializingMethod(StreamingContext context)
+        {
+            Member3 = "This value was set during deserialization";
+        }
+
+        [OnDeserialized]
+        internal void OnDeserializedMethod(StreamingContext context)
+        {
+            Member4 = "This value was set after deserialization.";
+        }
+    }
+
+}
--- Source/Test/WPF-SSSS/H.Test.Demo/MainViewModel.cs
+++ Source/Test/WPF-SSSS/H.Test.Demo/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using H.Providers.Mvvm;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace H.Test.Demo
 {
@@ -24,6 +26,21 @@ namespace H.Test.Demo
 
             this.MyGenericTypes.Add(new MyGenericClass<Int32>());
             this.MyGenericTypes.Add(new MyGenericClass<string>());
+
+            this.TaskCompletionSourceTests.Add(new TaskCompletionSourceTest());
+        }
+
+
+        private ObservableCollection<TaskCompletionSourceTest> _TaskCompletionSourceTests = new ObservableCollection<TaskCompletionSourceTest>();
+        /// <summary> 说明  </summary>
+        public ObservableCollection<TaskCompletionSourceTest> TaskCompletionSourceTests
+        {
+            get { return _TaskCompletionSourceTests; }
+            set
+            {
+                _TaskCompletionSourceTests = value;
+                RaisePropertyChanged("TaskCompletionSourceTests");
+            }
         }
 
 
@@ -39,6 +56,70 @@ namespace H.Test.Demo
             }
         }
 
+    }
+
+    public class TaskCompletionSourceTest : NotifyPropertyChangedBase
+    {
+
+        private string _message;
+        /// <summary> 说明  </summary>
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        TaskCompletionSource<bool> _taskCompletionSource;
+        CancellationTokenSource _tokenSource;
+        public RelayCommand SumitCommand => new RelayCommand((s, e) =>
+        {
+            this.Message += Environment.NewLine + "点击提交";
+            this.Result = true;
+            _taskCompletionSource.SetResult(true);
+        });
+
+        public bool Result { get; set; }
+
+        public RelayCommand CancelCommand => new RelayCommand((s, e) =>
+        {
+            this.Message += Environment.NewLine + "点击取消";
+            _tokenSource.Cancel();
+            _taskCompletionSource.SetResult(false);
+            this.Result = false;
+            //// 会报错
+            //_taskCompletionSource.SetCanceled();
+        });
+
+        public RelayCommand RunAsyncCommand => new RelayCommand(async (s, e) =>
+        {
+            this.Message += Environment.NewLine + "开始任务";
+            _taskCompletionSource = new TaskCompletionSource<bool>();
+            _tokenSource = new CancellationTokenSource();
+            await this.RunAsync(_tokenSource.Token);
+            this.Message += Environment.NewLine + "执行完成,结果：" + this.Result;
+        });
+
+        public RelayCommand ExceptionCommand => new RelayCommand((s, e) =>
+        {
+            this.Message += Environment.NewLine + "点击异常";
+            _taskCompletionSource.SetException(new Exception());
+        });
+
+        public async Task RunAsync(CancellationToken cancellationToken)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    break;
+                this.Message += Environment.NewLine + "步骤：" + i;
+                await Task.Delay(1000);
+            }
+            await _taskCompletionSource.Task;
+        }
     }
 
 }
