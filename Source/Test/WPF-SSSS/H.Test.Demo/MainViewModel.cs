04747016eed082184d2ed925905bbc292c4794cb
HeBianGu <908293466@qq.com>
2024-09-21 16:10:49 +0800

    添加图像绘制和保存功能
    在 `MainViewModel.cs` 中：
    - 引入了 `System.Windows.Media.Imaging` 等命名空间。
    - 在构造函数中调用 `DrawingContextWithWriteableBitmapExample` 方法。
    - 添加了 `_ImageSource` 字段和 `ImageSource` 属性，用于绑定图像源。
    - 添加了 `DrawingContextWithWriteableBitmapExample` 方法，演示如何使用 `DrawingVisual` 和 `DrawingContext` 绘制图形，并将其渲染到 `WriteableBitmap` 中。
    - 添加了 `SaveAsPng`、`SaveAsJpeg` 和 `SaveAsBmp` 方法，用于将 `RenderTargetBitmap` 保存为不同格式的图像文件。

--- Source/Test/WPF-SSSS/H.Test.Demo/MainViewModel.cs
+++ Source/Test/WPF-SSSS/H.Test.Demo/MainViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
 
 namespace H.Test.Demo
 {
@@ -28,6 +31,8 @@ namespace H.Test.Demo
             this.MyGenericTypes.Add(new MyGenericClass<string>());
 
             this.TaskCompletionSourceTests.Add(new TaskCompletionSourceTest());
+
+            this.DrawingContextWithWriteableBitmapExample();
         }
 
 
@@ -56,6 +61,103 @@ namespace H.Test.Demo
             }
         }
 
+
+        private ImageSource _ImageSource;
+        /// <summary> 说明  </summary>
+        public ImageSource ImageSource
+        {
+            get { return _ImageSource; }
+            set
+            {
+                _ImageSource = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        public void DrawingContextWithWriteableBitmapExample()
+        {
+            int width = 200;
+            int height = 200;
+            WriteableBitmap writeableBitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Pbgra32, null);
+
+            // 使用 DrawingVisual 和 DrawingContext 绘制图形
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                drawingContext.DrawRectangle(Brushes.Blue, null, new Rect(50, 50, 100, 100));
+                drawingContext.DrawText(
+                    new FormattedText(
+                        "Hello, WPF!",
+                        System.Globalization.CultureInfo.InvariantCulture,
+                        FlowDirection.LeftToRight,
+                        new Typeface("Verdana"),
+                        32,
+                        Brushes.White),
+                    new Point(60, 60));
+
+                drawingContext.DrawDrawing(new GeometryDrawing(Brushes.Red, new Pen(Brushes.Black, 1), new RectangleGeometry(new Rect(10, 10, 50, 50))));
+            }
+
+            // 将 DrawingVisual 渲染到 WriteableBitmap 中
+            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
+            renderTargetBitmap.Render(drawingVisual);
+
+            // 将 RenderTargetBitmap 的内容复制到 WriteableBitmap
+            writeableBitmap.Lock();
+            renderTargetBitmap.CopyPixels(new Int32Rect(0, 0, width, height), writeableBitmap.BackBuffer, writeableBitmap.BackBufferStride * height, writeableBitmap.BackBufferStride);
+            writeableBitmap.AddDirtyRect(new Int32Rect(0, 0, width, height));
+            writeableBitmap.Unlock();
+
+            // 将 WriteableBitmap 绑定到 Image 控件以显示图像
+            //Image image = new Image();
+            //image.Source = writeableBitmap;
+
+            //this.Content = image;
+            //this.Width = 250;
+            //this.Height = 250;
+
+            this.ImageSource = writeableBitmap;
+
+            // 保存为 PNG 文件
+            SaveAsPng(renderTargetBitmap, "output.png");
+
+            // 保存为 JPEG 文件
+            SaveAsJpeg(renderTargetBitmap, "output.jpg");
+
+            // 保存为 BMP 文件
+            SaveAsBmp(renderTargetBitmap, "output.bmp");
+        }
+
+        public void SaveAsPng(RenderTargetBitmap renderTargetBitmap, string filePath)
+        {
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+            using (var stream = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+            {
+                encoder.Save(stream);
+            }
+        }
+
+        public void SaveAsJpeg(RenderTargetBitmap renderTargetBitmap, string filePath)
+        {
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+            using (var stream = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+            {
+                encoder.Save(stream);
+            }
+        }
+
+        public void SaveAsBmp(RenderTargetBitmap renderTargetBitmap, string filePath)
+        {
+            BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+            using (var stream = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+            {
+                encoder.Save(stream);
+            }
+        }
     }
 
     public class TaskCompletionSourceTest : Bindable
