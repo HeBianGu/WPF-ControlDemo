38125e28047ddb0921394c0f225fd227918a76f8
HeBianGu <908293466@qq.com>
2025-08-11 00:01:05 +0800

    +

--- Source/Test/LicenseValidatorTask/LicenseValidatorTask.cs
+++ Source/Test/LicenseValidatorTask/LicenseValidatorTask.cs
@@ -12,17 +12,16 @@ namespace LicenseValidatorTask
         [Required]
         public string ModuleName { get; set; }
         [Required]
-        public string PublicKeyFilePath { get; set; }
-        [Required]
-        public string LicenseFilePath { get; set; }
-
+        public string BasePath { get; set; }
 
         public override bool Execute()
         {
             try
             {
-                string publicKey = File.ReadAllText(this.PublicKeyFilePath);
-                string license = File.ReadAllText(this.LicenseFilePath);
+                string publicKey = Path.Combine(this.BasePath, this.ModuleName, "default.pub");
+                publicKey = File.ReadAllText(publicKey);
+                string license = Path.Combine(this.BasePath, this.ModuleName, "license.lic");
+                license = File.ReadAllText(license);
                 LicenseService licenseService = new LicenseService();
                 LicenseOption option = licenseService.IsVail(publicKey, this.ModuleName, license, out string error);
                 if (option == null)
